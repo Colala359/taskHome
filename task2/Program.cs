0e18eb2a85d62fcff5fe40ced91350d0d719c956
Colala <antt3215@gmail.com>
2024-02-01 16:09:11 +0500

    add task2

---
+++ task2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        /*Задача 2: 
+            Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+            Даны два неотрицательных числа m и n.*/
+
+        int Ack(int m, int n)
+        {
+            if (m == 0) return n + 1;
+            else if (n == 0) return Ack(m - 1, 1);
+            else return Ack(m - 1, Ack(m, n - 1));
+        }
+
+
+        Console.Write("Введите значение M: ");
+        int M = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Введите значение N: ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        if (N < 0 || M < 0) Console.WriteLine("Неверное значение.");
+
+        Console.WriteLine(Ack(M, N));
+
+    }
+}
